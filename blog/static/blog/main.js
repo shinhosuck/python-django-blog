962a3990d0fe2e3f67b83ac7147b273f2876799f
shinhosuck <shinhosuck1973@gmail.com>
2023-02-13 06:30:52 +0800

    update

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -1,10 +1,14 @@
 const mobileMenuItems = document.querySelector('.mobile-menu-items')
-const barsBtn = document.querySelector('.fa-bars')
-const closeBtn = document.querySelector('.fa-close')
+const barsBtn = document.querySelector('.bars-btn')
+const closeBtn = document.querySelector('.close-btn')
 const mobileSearchBtn = document.querySelector('.mobile-search-btn')
 const mobileSearchForm = document.querySelector('.mobile-search-form')
+const navItems = document.querySelector('.nav-items')
+const barsBtn2 = document.querySelector('.bars-btn-2')
+const closeBtn2 = document.querySelector('.close-btn-2')
 
 
+console.log(closeBtn2)
 
 let clickEvents = function handleClickEvents(event) {
     
@@ -24,12 +28,28 @@ let clickEvents = function handleClickEvents(event) {
         event.preventDefault()
         mobileSearchForm.classList.toggle('show-mobile-search-form')
     }
+    if(event.currentTarget == barsBtn2) {
+        navItems.classList.toggle('show-nav-items')
+        barsBtn2.style.display = 'none'
+        closeBtn2.style.display = 'inline-block'
+    }
+    if(event.currentTarget == closeBtn2) {
+        navItems.classList.toggle('show-nav-items')
+        barsBtn2.style.display = 'inline-block'
+        closeBtn2.style.display = 'none'
+    }
     else if(event.target == window) {
-        if(window.innerWidth > 500) {
+        if(window.innerWidth > 780) {
             // document.body.style.overflow = "auto"
             closeBtn.style.display = 'none'
             barsBtn.style.display = 'inline-block'
             mobileMenuItems.classList.remove('show-mobile-menu-items')
+        }
+        if(window.innerWidth > 1150 || window.innerWidth === 780) {
+            // document.body.style.overflow = "auto"
+            closeBtn2.style.display = 'none'
+            barsBtn2.style.display = 'inline-block'
+            navItems.classList.remove('show-nav-items')
         }
     }
 }
@@ -41,3 +61,7 @@ closeBtn.addEventListener('click', clickEvents)
 window.addEventListener('resize', clickEvents)
 
 mobileSearchBtn.addEventListener('click', clickEvents)
+
+barsBtn2.addEventListener('click', clickEvents)
+
+closeBtn2.addEventListener('click', clickEvents)
